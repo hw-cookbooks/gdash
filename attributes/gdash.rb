1e82294d72dd1fba292ad5d0db5ffdd160cb8921
Laradji Nacer <nacer.laradji@gmail.com>
2012-07-11 14:08:27 +0200

    grpahite attribute listen_port don't seem to exist anymore

--- attributes/gdash.rb
+++ attributes/gdash.rb
@@ -7,7 +7,7 @@ default.gdash.url = "https://github.com/ripienaar/gdash/tarball/master"
 # TODO: put this in a databag ?
 ####
 include_attribute "graphite::graphite"
-default.gdash.graphite_url = "http://#{ipaddress}:#{graphite[:listen_port]}"
+default.gdash.graphite_url = "http://#{graphite[:url]}"
 ####
 
 default.gdash.templatedir = "/srv/gdash/graph_templates"
