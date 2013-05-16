02cbf3d3fd5f24b43347838c096edaeb8b9affde
Tim Smith <tsmith@llnw.com>
2013-05-15 17:55:49 -0700

    Don't use strings for the mode def

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,11 +26,11 @@ end
 gem_package "bundler"
 
 remote_file node.gdash.tarfile do
-  mode "00666"
   owner "www-data"
   group "www-data"
   source node.gdash.url
   action :create_if_missing
+  mode 0644
 end
 
 directory node.gdash.base do
