325e977b33bf6758185f2f5a28d60173eaca0433
Sean Escriva <sean.escriva@gmail.com>
2013-11-20 10:20:55 -0800

    Update metadata and changelog for release

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## 2013-11-20 -  v0.1.0
+
+Bump to v0.1.0, add a proper changelog
+include_recipe fix from Avishai
+Lots of fixes from Tim Smith
+
+## 2012-04-25 -  v0.0.2
+
+FileEdit patching
+proper unicorn/runit behavior
--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,13 @@ maintainer_email "ops@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures gdash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.1.0"
+name             "gdash"
 
 recipe  "gdash", "Main gdash setup and service configuration"
 recipe  "gdash::firewall", "Iptables rules. Optional, iptables cookbook required"
 recipe  "gdash::basic_dashboard", "Sets up simple dashboard, example LWRP usage"
+recipe  "gdash::attribute_driven_dashboard", "Data driven dashboard via attributes"
 
 %w{ debian ubuntu }.each do |os|
   supports os
