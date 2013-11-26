4c4fc312fa60cd373033b951ce186ba1144da5a7
Robert <taeil.choi@kt.com>
2013-11-26 17:13:29 +0900

    add 'graph_properties' attribute to dashboard resource.

--- resources/dashboard.rb
+++ resources/dashboard.rb
@@ -8,3 +8,4 @@ actions :create, :delete
 attribute :category, :kind_of => String, :required => true
 attribute :description, :kind_of => String, :required => true
 attribute :display_name, :kind_of => String, :required => false
+attribute :graph_properties, :kind_of => Hash, :required => false
