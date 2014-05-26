d184ffac6bbee06c7e51237e02fee3a651466ec6
Tim Smith <tsmith@llnw.com>
2014-05-26 12:21:37 -0700

    Minor tweaks

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 include_recipe 'build-essential'
 include_recipe 'unicorn'
 include_recipe 'runit'
